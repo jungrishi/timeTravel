7b5aee910c1a6ce36ce11c3111986d506e363419
rishijung <ranabhat.85@gmail.com>
2020-05-03 22:41:47 +0545

    Change port to 33507

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ class BaseConfig():
     SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
     LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
     LOG_PATH = os.getenv("LOG_PATH", "logs/timeTravel.log")
+    PORT = os.getenv("PORT", 33507)
     
 Config = BaseConfig()    
--- server.py
+++ server.py
@@ -28,4 +28,4 @@ def verify_slack_token():
 
 if __name__ == '__main__':
     application = create_app()
-    application.run(host='0.0.0.0', port=5000, debug=True)
+    application.run(host='0.0.0.0', port=Config.PORT, debug=True)
