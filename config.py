73196679236bfb21dc0596c2ec36ddfc0b75c1bb
rishijung <ranabhat.85@gmail.com>
2020-05-09 23:24:02 +0545

    Fix: pull environment var

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class BaseConfig():
     LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
     LOG_PATH = os.getenv("LOG_PATH", "logs/timeTravel.log")
     PORT = os.getenv("PORT", 33507)
-    SLACK_SIGNING_SECRET = os.environ("SLACK_SIGNING_SECRET", "")
+    SLACK_SIGNING_SECRET = os.getenv("SLACK_SIGNING_SECRET", "")
     
 Config = BaseConfig()    
