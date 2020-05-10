c3cbf50dc4ef589fcf92fe16cded4748d6133c0b
rishijung <ranabhat.85@gmail.com>
2020-05-10 23:34:56 +0545

    Add scheduler for slack client

--- resources/slackClient.py
+++ resources/slackClient.py
@@ -1,5 +1,5 @@
 import functools
-import os
+import re
 import time
 from logger import logger
 from datetime import timedelta, datetime
@@ -22,8 +22,9 @@ quote_payload = lambda dict: { k : quote_plus(str(v)) for k,v in dict.items()}
 def is_private(event):
     return event.get("channel").startswith('D')
 
-def get_mention():
-    return '@here'
+def get_time(message):
+    [arr_item] = re.findall(r'[0-9]+', message)
+    return time.time() + int(arr_item) * 60
 
 def send_response(user_mention):
     response_template = random.choice(['Sup, {mention}...',
@@ -70,13 +71,16 @@ def sender_decorator(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         print(result)
-        # updateMsg = time_travel_slack_client.chat_scheduleMessage(
-        #     channel=result['channel_id'],
-        #     text=result['text'],
-        #     post_at=result['time_stamp'],
-        #     as_user=True
-        # )
-        return result
+        try:
+            updateMsg = time_travel_slack_client.chat_scheduleMessage(
+            channel=result['channel_id'],
+            text=result['text'],
+            post_at=result['time_stamp'],
+            as_user=True
+        )
+            return {"Notification approved"}
+        except:
+            pass
     return wrapper
 
 @with_logging
@@ -85,7 +89,8 @@ def send_message(payload, timestamp, signature):
     hashed_signature = generate_signature(payload, timestamp)
     if not compare_signature(hashed_signature,signature):
         abort(HTTPStatus.METHOD_NOT_ALLOWED, message="Cannot Verify The App")
-        
+
     command, text, channel_id = pluck_payloads(payload, 'command', 'text', 'channel_id') 
-    print(command)
+    timestamp = get_time(text)
+    return {"channel_id":channel_id, "timestamp":timestamp, "text":text }
 
