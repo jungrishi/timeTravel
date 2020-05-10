adb0523ecb14f07b24f1524ae3bef9e4443f8850
rishijung <ranabhat.85@gmail.com>
2020-05-10 20:52:14 +0545

    Verify app with signing keys

--- resources/slackClient.py
+++ resources/slackClient.py
@@ -6,10 +6,39 @@ from datetime import timedelta, datetime
 from slack import WebClient
 from flask_restplus import abort
 from slackeventsapi import SlackEventAdapter
+import random
+import hashlib
+from http import HTTPStatus
+from operator import itemgetter
 
 from config import Config
 
 time_travel_slack_client = WebClient(Config.SLACK_BOT_TOKEN)
+
+pluck_payloads = lambda dict, *args: (dict[arg] for arg in args) #destructing the payloads to get required values from dict
+
+def is_private(event):
+    return event.get("channel").startswith('D')
+
+def get_mention():
+    return '@here'
+
+def send_response(user_mention):
+    response_template = random.choice(['Sup, {mention}...',
+                                       'Yo!',
+                                       'Hola {mention}',
+                                       'Bonjour!'])
+    
+    return response_template.format(mention=user_mention)
+
+def generate_signature(request_body, timestamp):
+    str_basestring = 'v0:' + timestamp + ':' + request_body
+    my_signature = 'v0=' + hashlib.sha256(Config.SLACK_SIGNING_SECRET, str_basestring).hexdigest()
+    return my_signature
+
+def compare_signature(hashed_signature, request_signature):
+    signature, salt = hashed_signature.split(':')
+    return signature == hashlib.sha256(salt.encode() + request_signature.encode()).hexdigest()
 
 def with_logging(func):
     @functools.wraps(func)
@@ -20,20 +49,26 @@ def with_logging(func):
         return result
     return wrapper
 
+def sender_decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print(result)
+        # updateMsg = time_travel_slack_client.chat_scheduleMessage(
+        #     channel=result['channel_id'],
+        #     text=result['text'],
+        #     post_at=result['time_stamp'],
+        #     as_user=True
+        # )
+        return result
+    return wrapper
+
 @with_logging
-def send_message(msg, future_time):
-    try:
-        updateMsg = time_travel_slack_client.chat_scheduleMessage(
-            channel="#welcome",
-            text=msg,
-            post_at=future_time,
-            as_user=True
-        )
-        logger.debug('print slack-api',updateMsg)
+@sender_decorator
+def send_message(payload, timestamp, signature):
+    hashed_signature = generate_signature(payload, timestamp)
+    if not compare_signature(hashed_signature,signature):
+        abort(HTTPStatus.METHOD_NOT_ALLOWED, HTTPStatus.METHOD_NOT_ALLOWED.phrase)
         
-        return {
-            "data": "Success"
-        }, 200
-    except:
-        abort(500, "Server Error")
+    command, text, channel_id = pluck_payloads(payload, 'command', 'text', 'channel_id') 
+    print(command)
 
--- resources/slash.py
+++ resources/slash.py
@@ -1,24 +1,44 @@
-from flask_restplus import Resource, abort
+from flask_restplus import Resource, abort, reqparse
 from flask import request
 from datetime import datetime, timedelta
 from slack.errors import SlackApiError
 from http import HTTPStatus
+from werkzeug.datastructures import ImmutableMultiDict, MultiDict
+import time
 
 from resources.slackClient import send_message
 from resources.api import api as slack_api
+
+
+parser = reqparse.RequestParser()
+parser =slack_api.parser()
+
+parser.add_argument('token', required = True, type=str, location = 'form')
+parser.add_argument('team_id', required = True, type=str, location = 'form')
+parser.add_argument('team_domain', required = True, type=str, location = 'form')
+parser.add_argument('channel_id', required = True, type=str, location = 'form')
+parser.add_argument('channel_name', required = True, type=str, location = 'form')
+parser.add_argument('user_id', required = True, type=str, location = 'form')
+parser.add_argument('user_name', required = True, type=str, location = 'form')
+parser.add_argument('command', required = True, type=str, location = 'form')
+parser.add_argument('text', required = True, type=str, location = 'form')
+parser.add_argument('response_url', required = True, type=str, location = 'form')
+parser.add_argument('trigger_id', required = True, type=str, location = 'form')
 
 @slack_api.route("/slack/test")
 class SlackTest(Resource):
     def get(self):
         return "Hello"
     
+    @slack_api.doc(parser=parser)
     def post(self):
-        print(request.form)
-        # info = request.form
-        current_time = datetime.now()
-        future_time = (current_time + timedelta(minutes=1)).timestamp()
-        msg = 'Welcome Scheduled Message In python'
+        payload = parser.parse_args()
+        timestamp = request.headers['X-Slack-Request-Timestamp']
+        signature = request.headers['X-Slack-Signature']
+        if abs(time.time() - timestamp) > 60 * 3:
+            abort(HTTPStatus.NOT_ACCEPTABLE, HTTPStatus.NOT_ACCEPTABLE.phrase)
         try:
-            return send_message(msg, future_time)
+            send_message(payload,timestamp, signature)
+            return payload
         except SlackApiError as error:
             abort(HTTPStatus.BAD_REQUEST, error)
