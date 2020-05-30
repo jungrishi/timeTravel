170f4be6468004d19cf9d01748fb72bb982c1049
rishijung <ranabhat.85@gmail.com>
2020-05-30 15:14:51 +0545

    add service to handle github request

--- resources/github_deploy_stage.py
+++ resources/github_deploy_stage.py
@@ -1,12 +1,13 @@
 from flask_restplus import Resource, abort
-
+from flask import request
 from slack.errors import SlackApiError
 from http import HTTPStatus
 from resources.api import api as github_api
+from services.githubClient import send_message
 
 @github_api.route("/slack/deploy-state")
 class GIthubWebhook(Resource):
     def get(self):
         return "Hello! POST for any meaningful resource"
     def post(self):
-        return "Hello"
+        return send_message(request.json)
---
+++ services/githubClient.py
@@ -0,0 +1,11 @@
+from decorators.logging import with_logging
+from bot import Bot
+
+@with_logging
+def send_message(payload):
+    print(payload)
+    parse_github_request(payload)
+    bot = Bot(payload)
+    
+def parse_github_request(payload):
+    pass    
